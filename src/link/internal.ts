e976088f5b8ee85bbafbe85217fe40be8f31ee65
piotrekwitkowski <piotrekwitkowski1@gmail.com>
2026-04-09 22:09:08 +0100

    fix(link): remove shadow root whitespace around <a> causing space before punctuation

--- src/link/internal.ts
+++ src/link/internal.ts
@@ -6,7 +6,6 @@ import { CsBaseElement } from '../internal/base-element.js';
 import { fireNonCancelableEvent } from '../internal/events.js';
 import { componentStyles, sharedStyles } from './styles.js';
 import type { LinkProps } from './interfaces.js';
-import '../icon/index.js';
 
 export class CsLinkInternal extends CsBaseElement {
   static override styles = [sharedStyles, componentStyles];
@@ -90,19 +89,16 @@ export class CsLinkInternal extends CsBaseElement {
       : nothing;
 
     if (isLink) {
-      return html`
-        <a
+      return html`<a
           class=${classMap(linkClasses)}
           href=${this.href}
           target=${ifDefined(this._resolvedTarget())}
           rel=${ifDefined(this._resolvedRel())}
           aria-label=${ifDefined(this.ariaLabel || undefined)}
-        ><slot></slot>${externalIcon}</a>
-      `;
+        ><slot></slot>${externalIcon}</a>`;
     }
 
-    return html`
-      <button
+    return html`<button
         class=${classMap(linkClasses)}
         type="button"
         aria-label=${ifDefined(this.ariaLabel || undefined)}
